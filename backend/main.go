3f5646cec0ff907d94388360eb9b05a1ca930d99
Poyi1028 <111306057@g.nccu.edu.tw>
2025-04-26 16:48:26 +0800

    建立專案結構

---
+++ backend/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello gin!",
+		})
+	})
+	r.Run() // 預設在瀏覽器 localhost:8080
+}
